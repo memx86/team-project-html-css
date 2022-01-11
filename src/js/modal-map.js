6b551b03d0993e3a050f2cae386e9cf9e8fa666c
Yevhen Malyshko <memx86@gmail.com>
2022-01-11 20:46:28 +0200

    new modal files

---
+++ src/js/modal-map.js
@@ -0,0 +1,37 @@
+// Задать кнопке открытия модального окна аттрибут data-modal-open="map"
+// Задать backdrop модального окна аттрибуты data-modal="map", class="is-hidden"
+// Задать кнопке закрытия модального окна аттрибут data-modal-close="map"
+// Модальное окно закрывается по клику на кнопку закрытия, по клику в бэкдроп, по нажатию клавиши "Esc"
+const refs = {
+  modal: document.querySelector('[data-modal="map"]'),
+  openBtn: document.querySelector('[data-modal-open="map"]'),
+  closeBtn: document.querySelector('[data-modal-close="map"]'),
+};
+
+refs.openBtn.addEventListener('click', onModalOpenBtnClick);
+
+function onModalOpenBtnClick() {
+  openModal();
+  refs.closeBtn.addEventListener('click', closeModal);
+  refs.modal.addEventListener('click', onBackdropClick);
+  document.addEventListener('keydown', onEscDown);
+}
+function openModal() {
+  refs.modal.classList.remove('is-hidden');
+  document.body.classList.add('modal-open');
+}
+function closeModal() {
+  refs.modal.classList.add('is-hidden');
+  document.body.classList.remove('modal-open');
+  refs.closeBtn.removeEventListener('click', closeModal);
+}
+function onBackdropClick(e) {
+  if (e.target !== refs.modal) return;
+  closeModal();
+  refs.modal.removeEventListener('click', onBackdropClick);
+}
+function onEscDown(e) {
+  if (e.code !== 'Escape') return;
+  closeModal();
+  document.removeEventListener('keydown', onEscDown);
+}
