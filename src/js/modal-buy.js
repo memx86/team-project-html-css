ee0561c6ba74734027a55caec586a21ae7fe0cc9
Yevhen Malyshko <memx86@gmail.com>
2022-01-12 22:04:43 +0200

    createModal function

--- src/js/modal-buy.js
+++ src/js/modal-buy.js
@@ -3,38 +3,11 @@
 // Задать backdrop модального окна аттрибуты data-modal="buy", class="is-hidden"
 // Задать кнопке закрытия модального окна аттрибут data-modal-close="buy"
 // Модальное окно закрывается по клику на кнопку закрытия, по клику в бэкдроп, по нажатию клавиши "Esc"
+import createModal from './modal';
 const refs = {
   modal: document.querySelector('[data-modal="buy"]'),
   openBtn: document.querySelector('[data-modal-open="buy"]'),
   openBtnMobileMenu: document.querySelector('[data-modal-open="buy-mobile-menu"]'),
   closeBtn: document.querySelector('[data-modal-close="buy"]'),
 };
-
-refs.openBtn.addEventListener('click', onModalOpenBtnClick);
-refs.openBtnMobileMenu.addEventListener('click', onModalOpenBtnClick);
-
-function onModalOpenBtnClick() {
-  openModal();
-  refs.closeBtn.addEventListener('click', closeModal);
-  refs.modal.addEventListener('click', onBackdropClick);
-  document.addEventListener('keydown', onEscDown);
-}
-function openModal() {
-  refs.modal.classList.remove('is-hidden');
-  document.body.classList.add('modal-open');
-}
-function closeModal() {
-  refs.modal.classList.add('is-hidden');
-  document.body.classList.remove('modal-open');
-  refs.closeBtn.removeEventListener('click', closeModal);
-  refs.modal.removeEventListener('click', onBackdropClick);
-  document.removeEventListener('keydown', onEscDown);
-}
-function onBackdropClick(e) {
-  if (e.target !== refs.modal) return;
-  closeModal();
-}
-function onEscDown(e) {
-  if (e.code !== 'Escape') return;
-  closeModal();
-}
+createModal(refs);
--- src/js/modal-map.js
+++ src/js/modal-map.js
@@ -2,36 +2,10 @@
 // Задать backdrop модального окна аттрибуты data-modal="map", class="is-hidden"
 // Задать кнопке закрытия модального окна аттрибут data-modal-close="map"
 // Модальное окно закрывается по клику на кнопку закрытия, по клику в бэкдроп, по нажатию клавиши "Esc"
+import createModal from './modal';
 const refs = {
   modal: document.querySelector('[data-modal="map"]'),
   openBtn: document.querySelector('[data-modal-open="map"]'),
   closeBtn: document.querySelector('[data-modal-close="map"]'),
 };
-
-refs.openBtn.addEventListener('click', onModalOpenBtnClick);
-
-function onModalOpenBtnClick() {
-  openModal();
-  refs.closeBtn.addEventListener('click', closeModal);
-  refs.modal.addEventListener('click', onBackdropClick);
-  document.addEventListener('keydown', onEscDown);
-}
-function openModal() {
-  refs.modal.classList.remove('is-hidden');
-  document.body.classList.add('modal-open');
-}
-function closeModal() {
-  refs.modal.classList.add('is-hidden');
-  document.body.classList.remove('modal-open');
-  refs.closeBtn.removeEventListener('click', closeModal);
-  refs.modal.removeEventListener('click', onBackdropClick);
-  document.removeEventListener('keydown', onEscDown);
-}
-function onBackdropClick(e) {
-  if (e.target !== refs.modal) return;
-  closeModal();
-}
-function onEscDown(e) {
-  if (e.code !== 'Escape') return;
-  closeModal();
-}
+createModal(refs);
