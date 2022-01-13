5fad88e70ac350d8e3dcbb01861a502764c3514d
Selim Fazylov <selim.fazylov1@gmail.com>
2022-01-13 12:19:04 +0200

    подключил модалки
    исправил js, подключил модалки в индекс, подключил свойства в модуль.scss,

--- src/js/modal-coffee.js
+++ src/js/modal-coffee.js
@@ -4,8 +4,8 @@
 // Модальное окно закрывается по клику на кнопку закрытия, по клику в бэкдроп, по нажатию клавиши "Esc"
 import createModal from './modal';
 const refs = {
-  modal: document.querySelector('[data-modal="coffee"]'),
-  openBtn: document.querySelector('[data-modal-open="coffee"]'),
-  closeBtn: document.querySelector('[data-modal-close="coffee"]'),
+  modal: document.querySelector('[data-modal="icecoffee"]'),
+  openBtn: document.querySelector('[data-modal-open="icecoffee"]'),
+  closeBtn: document.querySelector('[data-modal-close="icecoffee"]'),
 };
 createModal(refs);
